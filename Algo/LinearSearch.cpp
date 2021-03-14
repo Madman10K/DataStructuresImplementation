9b9c3c0fc59882ec189e238e4b954cfb22dbe6b1
4ipsdaqde6 <4ipsdaqde6@gmail.com>
2021-03-14 21:28:07 +0200

    Yes

---
+++ Algo/LinearSearch.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+short linear_search(int* arr, int val, size_t arr_size)
+{	
+	for (int i = 0; i < arr_size; i++)
+	{		
+		if (arr[i] == val)
+		{
+			
+			
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int main()
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	short found = linear_search(arr, 7, 10);
+
+	if (found)
+	{
+		printf("Found 7");
+	}
+	else
+	{
+		printf("Couldn't find 7");
+	}
+}
